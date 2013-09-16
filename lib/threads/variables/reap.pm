359aba7cfca2f5ea1ed60c841034e6f02d2bdce5
Jens Rehsack <sno@netbsd.org>
2013-09-16 10:01:59 +0200

    bump version to 0.06

--- lib/threads/variables/reap.pm
+++ lib/threads/variables/reap.pm
@@ -8,7 +8,7 @@ our @ISA       = qw(Exporter);
 our @EXPORT    = qw(reap);
 our @EXPORT_OK = qw(reap reapref);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Scalar::Util qw(weaken reftype);
 my @reapem;
